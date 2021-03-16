092a8ff9204c80c0cf265571d76205e55776604e
Juan Patrón <josejuanpatron1630@gmail.com>
2021-03-15 22:42:51 -0600

    Se agrego el envio del token por los headers

--- config/apollo.js
+++ config/apollo.js
@@ -1,5 +1,29 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, createHttpLink ,InMemoryCache } from '@apollo/client'
 import fetch  from 'node-fetch';
+// esta librerìa sirve para hacer las peticiones por via headers
+
+import { setContext } from 'apollo-link-context';
+
+
+const httpLink = createHttpLink({
+    // configuramos nuestro access point
+    uri: 'http://localhost:4000/',
+    fetch
+});
+
+
+const authLink = setContext( (_, { headers })=> {
+
+    //leer el storage almacenado
+        const token = localStorage.getItem('token')
+    return {  
+            // agregamos nuestro header a los demas headers 
+            headers:{
+                ...headers,
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        }
+});
 
 // apollo server es solo para node donde se declaran los modelos
 // node fetch nos permite traer los resultados  y pintarlos en los componentes
@@ -7,11 +31,7 @@ import fetch  from 'node-fetch';
 const client = new ApolloClient({
     connectToDevTools: true,
     cache : new InMemoryCache(),
-    link: new  HttpLink({
-        uri: 'http://localhost:4000/',
-        fetch
-    })
-
+    link: authLink.concat( httpLink )
 });
 
 
